4dbdb4326be2c6f8c91d7077d29dff4c67ed7733
Govind Sharma <53191779+Govind93@users.noreply.github.com>
2023-12-14 14:44:55 +0530

    Update Anova_Python.py

--- Anova_Python.py
+++ Anova_Python.py
@@ -1,35 +1,11 @@
-#2. Analysis of Variance (ANOVA) Test (for comparing more than two groups): (normal data)
-import numpy as np
-group1= [89,89,88,78,78];
-group2= [93,92,94,89,88];
-group3= [89,88,89,93,90];
-group4=[81,78,81,92,82];
-
-mean1 = np.mean(group1)
-sum1 = np.sum(group1)
-Var1 = np.var(group1)
-
-from scipy.stats import f_oneway
-# Assuming 'group1', 'group2', and 'group3' are your three groups
-statistic_F, p_value = f_oneway(group1, group2, group3, group4)
-# Check the p-value to determine if there is a significant difference
-if p_value < 0.05:
-    print("There is a significant difference between groups.")
-else:
-    print("There is no significant difference between groups.")
-
-print(f"F-statistic: {statistic_F}")
-print(f"p-value: {p_value}")
-
+# Analysis of Variance (ANOVA) Test (for comparing more than two groups): (normal data)
 #=======================================================================================================
-
-
 ## step by step Anova Calculation :
 import numpy as np
 from scipy.stats import f
 
-# Sample data
-#data = pd.read_csv('data.csv')
+# Sample data 
+## data = pd.read_csv('data.csv') # instead of Array we can read our CSV file.
 data = np.array([[89,89,88,78,78], [93,92,94,89,88], [89,88,89,93,90], [81,78,81,92,82]])
 
 # Step 1: Calculate overall mean
